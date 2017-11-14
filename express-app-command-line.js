f9d42405a47db0e47fc000524ae487f5e758047b
Claire Martinez <Claire@users.noreply.github.com>
2017-11-13 20:15:51 -0500

    Updating express-app-command-line.js

--- express-app-command-line.js
+++ express-app-command-line.js
@@ -13,8 +13,8 @@ app.get("/", (req, res) => {
         destUrl: "https://team.quickbase.com/db/main?a=QBI_logtrack&",
     });
 
-    res.set("Content-Type", "text/html");
-    res.status(200).send(HTML);
+res.set("Content-Type", "text/html");
+res.status(200).send(HTML);
 });
 
 function renderView(locals) {
@@ -24,25 +24,30 @@ function renderView(locals) {
     <head>
       <meta charset="utf-8">
       <title>${locals.title}</title>
-      <script src="https://unpkg.com/axios/dist/axios.min.js"></script>        
-  </form>
+      <script src="https://unpkg.com/axios/dist/axios.min.js"></script> 
     </head>
 
     <body>
-      ${locals.body}
+      <h1>Test Post Cors from Form Sumbit and Ajax Call</h1>
       <form method="POST" action="${locals.destUrl}">
-      <input type="text" name="lg" />
-      <input type="submit" />
-     <button onclick="postViaJs()">By Js</button>
-
+        Enter anything here <input type="text" name="lg" />
+        <br/>
+        <br/>
+        Click Submit button for form Post <input type="submit" />
+        Or click Ajax button for Post via ajax call <button onclick="postViaJs()">Ajax</button>
+        <p>Sends to ${locals.destUrl}</p>
+      </form>
+      
       <script> function postViaJs() { 
+        var urlForAjax = "${locals.destUrl}" + "&fromAjax=true";
+        var inputValue = document.getElementsByName('lg')[0].value;
         var requestConfig = { 
-          url : "${locals.destUrl}", 
+          url : urlForAjax, 
           method: 'post', 
-          data: {lg: document.getElementsByName('lg')[0].value},
-           withCredentials: true 
-          }; 
-          axios(requestConfig)
+          data: {lg: inputValue },
+          withCredentials: true 
+        }; 
+        axios(requestConfig)
           .then(function(data) { 
               console.log("success result = " + JSON.stringify(data))
            }) 
