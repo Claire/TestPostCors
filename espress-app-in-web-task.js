dbbde006d3a0ab7e5b28dbf67ecc1a14c22ef948
Claire Martinez <Claire@users.noreply.github.com>
2017-11-13 20:11:58 -0500

    Updating espress-app-in-web-task.js

--- espress-app-in-web-task.js
+++ espress-app-in-web-task.js
@@ -1,51 +1,53 @@
-"use latest";
+'use latest';
 
 import express from 'express';
 import { fromExpress } from 'webtask-tools';
 import bodyParser from 'body-parser';
+
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-    const HTML = renderView({
-        title: "Test Post Cors from Form Sumbit and Ajax",
-        body: "<h1>Test Post Cors from Form Sumbit and Ajax Call</h1>",
-        destUrl: "https://team.quickbase.com/db/main?a=QBI_logtrack&xml=true",
-    });
+app.get('/', (req, res) => {
+  const HTML = renderView({
+    title: 'Test Post Cors from Form Sumbit and Ajax',
+    destUrl: 'https://team.quickbase.com/db/main?a=QBI_logtrack&xml=true'
+  });
 
-    res.set("Content-Type", "text/html");
-    res.status(200).send(HTML);
+  res.set('Content-Type', 'text/html');
+  res.status(200).send(HTML);
 });
 
 module.exports = fromExpress(app);
 
 function renderView(locals) {
-    return `
+  return `
     <!DOCTYPE html>
     <html>
     <head>
       <meta charset="utf-8">
       <title>${locals.title}</title>
-      <script src="https://unpkg.com/axios/dist/axios.min.js"></script>        
-  </form>
+      <script src="https://unpkg.com/axios/dist/axios.min.js"></script> 
     </head>
 
     <body>
-      ${locals.body}
+      <h1>Test Post Cors from Form Sumbit and Ajax Call</h1>
       <form method="POST" action="${locals.destUrl}">
-      Enter anything here <input type="text" name="lg" />
-      <br/><br/>
-      Click Submit button for form Post <input type="submit" />
-      Or click Ajax button for Post via ajax call <button onclick="postViaJs()">Ajax</button>
-      <p>Sends to ${locals.destUrl}</p>
-     </form>
+        Enter anything here <input type="text" name="lg" />
+        <br/>
+        <br/>
+        Click Submit button for form Post <input type="submit" />
+        Or click Ajax button for Post via ajax call <button onclick="postViaJs()">Ajax</button>
+        <p>Sends to ${locals.destUrl}</p>
+      </form>
+      
       <script> function postViaJs() { 
-       var urlForAjax = "${locals.destUrl}" + "&fromAjax=true"
+        var urlForAjax = "${locals.destUrl}" + "&fromAjax=true";
+        var inputValue = document.getElementsByName('lg')[0].value;
         var requestConfig = { 
           url : urlForAjax, 
           method: 'post', 
-          data: {lg: document.getElementsByName('lg')[0].value},
+          data: {lg: inputValue },
           withCredentials: true 
         }; 
         axios(requestConfig)
@@ -62,4 +64,3 @@ function renderView(locals) {
     </html>
   `;
 }
-
